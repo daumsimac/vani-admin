8d464cad91c1baf12129eaeb7cc85869e4f90b7d
daumsimac <daumsimac@gmail.com>
2023-03-03 15:19:46 +0900

    Add oracle related
    Add drivers and setting with transaction history model

---
+++ src/main/java/com/vani/web/dto/TrHistoryListDto.java
@@ -0,0 +1,18 @@
+package com.vani.web.dto;
+
+public class TrHistoryListDto {
+    private String orgC;
+    private String svcId;
+    private String fromDate;
+    private String toDate;
+    private String debitBnkId;
+    private String debitRsrcNum;
+    private String crdBnkId;
+    private String crdRsrcNum;
+    private String cliMsgId;
+    private String gblid;
+    private String tranAm;
+    private String rspC;
+    private String tmoOcuYn;
+    private String trprcStsc;
+}
